024575bdfe4cd31629a44e223d9c35735e2a9607
Jean-Baptiste Tâm MATHIEU <jbtmathieu@gmail.com>
2019-07-01 16:41:29 +0200

    2eme commit2eme commit

--- sirh-gestion-personnel/src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ sirh-gestion-personnel/src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -1,31 +1,33 @@
 package dev.sgp.web;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ListerCollaborateursController extends HttpServlet{
+public class ListerCollaborateursController extends HttpServlet {
+
+	protected void doGet2(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().write("Hello ListerCollaborateursController!!");
+		// récupère la valeur d'un paramètre dont le nom est avecPhoto
+		String avecPhotoParam = req.getParameter("avecPhoto");
+		// récupère la valeur d'un paramètre dont le nom est département
+		String departementParam = req.getParameter("departement");
+		resp.setContentType("text/html");
+
+		// code HTML ecrit dans le corps de la réponse
+		resp.getWriter().write("<h1>Liste des collaborateurs</h1>" + "<ul>" + "<li>avecPhoto=" + avecPhotoParam
+				+ "</li>" + "<li>avecDepartement=" + departementParam + "</li>" + "<ul>");
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().write("Hello ListerCollaborateursController!!");
-		//récupère la valeur d'un paramètre dont le nom est avecPhoto
-		String avecPhotoParam =req.getParameter("avecPhoto");
-		//récupère la valeur d'un paramètre dont le nom est département
-		String departementParam =req.getParameter("departement");
-		resp.setContentType("text/html");
-		
-		//code HTML ecrit dans le corps de la réponse
-		resp.getWriter().write("<h1>Liste des collaborateurs</h1>"
-		+"<ul>"
-		+"<li>avecPhoto="+ avecPhotoParam + "</li>"
-		+"<li>avecDepartement="+ departementParam + "</li>"
-		+"<ul>");
-		
+		// A ajouter
+		req.setAttribute("listeNoms", Arrays.asList("Robert", "Jean", "Hugues"));
+		req.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp").forward(req, resp);
 	}
-	
 
 }
